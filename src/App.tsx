68c03f504de055d82ce62e7c154a0da16fc42eb0
anbrme <61997613+anbrme@users.noreply.github.com>
2025-07-24 01:33:33 +0200

    Revert "improvements"
    This reverts commit 92e7b4e23f718243132fb472af1f3fa90f0c199c.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { EnhancedDashboard } from './components/EnhancedDashboard';
-import './index.css';
 
 function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50">
-      <EnhancedDashboard />
-    </div>
-  );
+  return <EnhancedDashboard />;
 }
 
 export default App;
