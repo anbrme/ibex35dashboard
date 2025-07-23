3449063e9d4bedc9b1e9b884a54a49e7109fa956
anbrme <61997613+anbrme@users.noreply.github.com>
2025-07-24 01:44:01 +0200

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { EnhancedDashboard } from './components/EnhancedDashboard';
+import './index.css';
 
 function App() {
-  return <EnhancedDashboard />;
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-50 via-blue-50 to-indigo-50">
+      <EnhancedDashboard />
+    </div>
+  );
 }
 
 export default App;
